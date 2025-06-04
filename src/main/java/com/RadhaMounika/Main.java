12ffb52a81f0fc8b3de134ac2487e90ee36010ce
Radha Mounika <radha@Mac.lan>
2025-06-04 12:06:33 +0530

    initial commit

---
+++ src/main/java/com/RadhaMounika/Main.java
@@ -0,0 +1,11 @@
+package com.RadhaMounika;
+
+
+public class Main {
+    public static void main(String[] args) throws IllegalAccessException {
+        System.out.printf("Hello and welcome!");
+        System.out.println("ans ->" + Radha.getNullSafeValue("faff", "fafa"));
+    }
+
+
+}
